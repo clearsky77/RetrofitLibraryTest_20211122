5208d028f82299e7109622fe9a6f9f2d217425ec
clearsky77 <clearsky3264@gmail.com>
2021-11-22 21:52:12 +0900

    BaseActivity 기본 작업. 메인이 상속 받은 후 implement

--- app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/MainActivity.kt
+++ app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/MainActivity.kt
@@ -5,12 +5,20 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.clearsky77.retrofitlibrarytest_20211122.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding // xml에 <layout>달아야 찾아진다.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
     }
 }
