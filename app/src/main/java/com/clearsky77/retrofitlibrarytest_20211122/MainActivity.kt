724cf52e9f8767dfef50ec413d913b8d868941b6
clearsky77 <clearsky3264@gmail.com>
2021-11-23 00:07:23 +0900

    서버 응답 isSuccessful로 성공 여부 구분  - 로그인 실패 (code : 200 이외) 경우는 JSONObject만들어서 활용

--- app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/MainActivity.kt
+++ app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.clearsky77.retrofitlibrarytest_20211122
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.clearsky77.retrofitlibrarytest_20211122.databinding.ActivityMainBinding
 import com.clearsky77.retrofitlibrarytest_20211122.datas.BasicResponse
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -30,8 +32,21 @@ class MainActivity : BaseActivity() {
                     call: Call<BasicResponse>,
                     response: Response<BasicResponse>
                 ) {
-                    val basicResponse = response.body()!!
-                    Toast.makeText(mContext, basicResponse.message, Toast.LENGTH_SHORT).show()
+                    if (response.isSuccessful) {
+                        val basicResponse =  response.body()!!
+
+                        Toast.makeText(mContext, basicResponse.message, Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+
+                        val errorJson = JSONObject( response.errorBody()!!.string() )
+                        Log.d("에러경우", errorJson.toString())
+
+                        val message = errorJson.getString("message")
+
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+                    }
                 }
 
                 override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
