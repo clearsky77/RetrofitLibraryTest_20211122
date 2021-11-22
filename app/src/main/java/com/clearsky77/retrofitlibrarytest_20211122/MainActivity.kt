550808e66432a0d52245db9b3e90dbaa93c374f1
clearsky77 <clearsky3264@gmail.com>
2021-11-22 21:43:06 +0900

    xml과 kt 연결 : 데이터바인딩. 1.gradle 2.xml 3.kt 순으로 편집

--- app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/MainActivity.kt
+++ app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/MainActivity.kt
@@ -2,10 +2,15 @@ package com.clearsky77.retrofitlibrarytest_20211122
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.clearsky77.retrofitlibrarytest_20211122.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding // xml에 <layout>달아야 찾아진다.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
