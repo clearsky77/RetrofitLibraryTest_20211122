27b7b27ca29b0e9deac111074c7107cbe037611a
clearsky77 <clearsky3264@gmail.com>
2021-11-23 09:53:52 +0900

    회원가입 API 호출 - (4) 화면에서 API 호출  - 응답 돌아오는 코드 추가, 아직 행동 작성 X

--- app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/SignUpActivity.kt
+++ app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/SignUpActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.clearsky77.retrofitlibrarytest_20211122.databinding.ActivitySignUpBinding
+import com.clearsky77.retrofitlibrarytest_20211122.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : BaseActivity() {
 
@@ -22,6 +26,20 @@ class SignUpActivity : BaseActivity() {
             val email = binding.edtEmail.text.toString()
             val password = binding.edtPassword.text.toString()
             val nickname = binding.edtNickname.text.toString()
+
+            apiService.putRequestS(email, password, nickname).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            })
 
         }
 
