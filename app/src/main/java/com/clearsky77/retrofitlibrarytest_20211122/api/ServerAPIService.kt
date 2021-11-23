815fe5941af1a4f5a4bfcea4de421d3fc65c902b
clearsky77 <clearsky3264@gmail.com>
2021-11-23 09:50:45 +0900

    회원가입 API 호출 - (3) API 호출 함수 추가

--- app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/api/ServerAPIService.kt
+++ app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/api/ServerAPIService.kt
@@ -5,16 +5,25 @@ import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface ServerAPIService { // 왜 인터페이스? Retrofit에 create메소드가 그렇게 받기 때문에
 
-//     로그인 기능
+    //     로그인 기능
     @FormUrlEncoded// post, put, patch는 추가해줘야한다.
     @POST("/user")
     fun postRequestLogin(
         @Field("email") email: String,
         @Field("password") pw: String,
-    ) : Call<BasicResponse> // 호출 답으로 뭘 받을지 명시
+    ): Call<BasicResponse> // 호출 답으로 뭘 받을지 명시
 
+    //    회원가입 기능
+    @FormUrlEncoded// post, put, patch는 추가해줘야한다.
+    @PUT("/user")
+    fun putRequestS(
+        @Field("email") email: String, // form data
+        @Field("password") pw : String,
+        @Field("nick_name") nick: String,
+    ) : Call<BasicResponse>
 
 }
