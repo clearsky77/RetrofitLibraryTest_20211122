7137d7ffdeecc643d9f1661e3e62e6bbbe7195b1
clearsky77 <clearsky3264@gmail.com>
2021-11-23 11:29:00 +0900

    비밀번호 확인까지 실제로 검사. 부적합하면 가입 버튼 실행안됨.

--- app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/SignUpActivity.kt
+++ app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/SignUpActivity.kt
@@ -15,6 +15,8 @@ class SignUpActivity : BaseActivity() {
 
     lateinit var binding : ActivitySignUpBinding
     var isDuplOk = false
+    var isPasswordLenthOk = false
+    var isPasswordSame = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,12 +27,29 @@ class SignUpActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+//        1. 비밀번호 타이핑 이벤트 => 8글자 이상인지 아닌
+        binding.edtPassword.addTextChangedListener {
+            if(it.toString().length>=8) {
+                binding.txtPasswordCheckResult1.text = "사용해도 좋은 비밀번호입니다."
+            }else{
+                binding.txtPasswordCheckResult1.text = "8자 이상으로 해주세요"
+            }
+            isPasswordSame = compareTwoPasswords()
+        }
+
+        binding.edtPasswordRepeat.addTextChangedListener {
+            isPasswordSame = compareTwoPasswords()
+        }
+
+
         binding.edtEmail.addTextChangedListener {
 //            Log.d("입력된 내용", it.toString())
             // 재검사 요청
             binding.txtEmailCheckResult.text = "이메일 중복검사를 해주세요."
+            isDuplOk = false
         }
 
+//        이메일 중복확인 버튼
         binding.btnEmailCheck.setOnClickListener {
 
             val email = binding.edtEmail.text.toString()
@@ -56,10 +75,21 @@ class SignUpActivity : BaseActivity() {
 
         }
 
+//        회원가입 버튼
         binding.btnSignUp.setOnClickListener {
 
             if(!isDuplOk){
                 Toast.makeText(mContext, "이메일 중복검사를 해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if(!isPasswordLenthOk){
+                Toast.makeText(mContext, "비밀번호는 8글자 이상이어야합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if(!isPasswordSame){
+                Toast.makeText(mContext, "두 개의 비밀번호는 같아야합니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
@@ -98,4 +128,22 @@ class SignUpActivity : BaseActivity() {
 
     override fun setValues() {
     }
+
+// 비밀번호 검사 메소드
+    fun compareTwoPasswords() : Boolean{
+        val originalPassword = binding.edtPassword.text.toString()
+        val repeatPassword = binding.edtPasswordRepeat.text.toString()
+
+        if (originalPassword == repeatPassword) {
+            binding.txtPasswordCheckResult2.text = "사용해도 좋습니다."
+            return true
+        }
+        else {
+            binding.txtPasswordCheckResult2.text = "위의 비밀번호와 일치해야 합니다."
+            return false
+        }
+
+
+    }
+
 }
