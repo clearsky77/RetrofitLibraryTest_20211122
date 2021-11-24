d8cb3ef0418209d699474a00fd928318e6391c24
clearsky77 <clearsky3264@gmail.com>
2021-11-24 10:42:26 +0900

    프래그먼트들도 서버 호출 apiService 상속

--- app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/fragments/BaseFragment.kt
+++ app/src/main/java/com/clearsky77/retrofitlibrarytest_20211122/fragments/BaseFragment.kt
@@ -3,14 +3,20 @@ package com.clearsky77.retrofitlibrarytest_20211122.fragments
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.clearsky77.retrofitlibrarytest_20211122.api.ServerAPI
+import com.clearsky77.retrofitlibrarytest_20211122.api.ServerAPIService
 
 abstract class BaseFragment : Fragment() {
 
     lateinit var mContext: Context
+    lateinit var apiService: ServerAPIService
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         mContext = requireContext()
+
+        val retrofit = ServerAPI.getRetrofit() // 레트로핏 줘
+        apiService = retrofit.create(ServerAPIService::class.java)
     }
 
     abstract fun setupEvents()
